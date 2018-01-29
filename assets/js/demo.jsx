a38305b14d62ad9f6dd04a103fbd705b69b915a3
Omkar Bhat <omkar1496@gmail.com>
2018-01-29 01:09:00 -0500

    Game

--- assets/js/demo.jsx
+++ assets/js/demo.jsx
@@ -2,49 +2,145 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Button } from 'reactstrap';
 
-export default function run_demo(root) {
-  ReactDOM.render(<Demo side={0}/>, root);
+
+function Card(props) {
+  let show = ""
+
+  if (props.show){
+    show = ""
+  } else if (props.matched.includes(props.letter)) {
+    show = 'Matched'
+  } else {
+    show = 'hidden'
+  }
+
+  return (
+      <div className='box' onClick={() => props.flip(props.card)}>
+        <h1 className={show}>{props.letter}</h1>
+      </div>
+  );
 }
 
-class Demo extends React.Component {
+class MemoryGame extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { side: props.side };
+    this.state = {
+      clicks: 0,
+      moveOne: {
+        idx: -1,
+        flipped: false
+      },
+      moveTwo: {
+        idx: -1,
+        flipped: false
+      },
+      letters: this.randomizeBoard(),
+      matched: [],
+    }
+
+    this.resetBoard = this.resetBoard.bind(this);
+    this.flip = this.flip.bind(this)
+  };
+
+  randomizeBoard() {
+    const letters = "AABBCCDDEEFFGGHH";
+    return _.shuffle(letters.split(''));
   }
 
-  toggle(side) {
-    var side = +!this.state.side;
-    this.setState({side: side});
+
+  checkCard() {
+    const { moveOne, moveTwo, letters, matched } = this.state;
+    var localMatch = matched
+    if (letters[moveTwo.idx] == letters[moveOne.idx]) {
+      localMatch.push(letters[moveTwo.idx])
+
+    }
+
+  // Inspiration from https://stackoverflow.com/questions/36270422/reactjs-settimeout-not-working
+    setTimeout(() => {
+      this.setState({
+        matched: localMatch,
+        moveOne: {
+          idx: -1,
+          flipped: false
+        },
+        moveTwo: {
+          idx: -1,
+          flipped: false
+        },
+      });
+    }, 1500);
   }
+
+  resetBoard() {
+    this.setState = ({
+      clicks: 0,
+      moveOne: {
+        idx: -1,
+        flipped: false,
+      },
+      moveTwo: {
+        idx: -1,
+        flipped: false,
+      },
+      letters: this.randomizeBoard(),
+      matched: [],
+    })
+  }
+
+  flip(card) {
+    const {moveOne, moveTwo, clicks} = this.state;
+
+    if (!moveOne.flipped) {
+      this.setState({
+        moveOne: {
+          idx: card,
+          flipped: true
+        },
+        clicks: clicks + 1
+      })
+    } else if (!moveTwo.flipped) {
+      this.setState({
+        moveTwo: {
+          idx: card,
+          flipped: true
+        },
+        clicks: clicks + 1
+      }, this.checkCard)
+    }
+  }
+
+  buildBoard(letters) {
+    var rows = []
+    for (var idx = 0; idx < letters.length; idx++) {
+      rows.push(<Card
+        letter={letters[idx]}
+        card={idx}
+        flip={this.flip}
+        matched={this.state.matched}
+        show={this.state.moveOne.idx === idx || this.state.moveTwo.idx === idx}/>)
+    }
+    return rows
+  }
+
 
   render() {
-    var toggle = this.toggle.bind(this);
     return (
-      <div className="row">
-        <Side show={this.state.side == 0} toggle={toggle} />
-        <div className="col">
-          &nbsp;
-        </div>
-        <Side show={this.state.side == 1} toggle={toggle} />
+      <div>
+          <div>
+            <h3>{"Score:" + this.state.clicks}</h3>
+          </div>
+
+          <div className='row'>
+            {this.buildBoard(this.state.letters)}
+          </div>
+
+          <Button onClick={this.reset}>Reset</Button>
       </div>
-    );
+    )
   }
 }
 
-function Side(params) {
-  if (params.show) {
-    return (
-      <div id="side-0" className="side col" onMouseOver={ () => params.toggle() }>
-        <Button onClick={ () => alert("cheater") }>Click Me</Button>
-      </div>
-    );
-  }
-  else {
-    return (
-      <div id="side-0" className="side col">
-        &nbsp;
-      </div>
-    );
-  }
+export default function run_game(root) {
+  ReactDOM.render(<MemoryGame />, root);
 }
-
